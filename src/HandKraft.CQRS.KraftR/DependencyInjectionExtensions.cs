298cc1f4f2bfe4749e6fa71197ee69ad902417e1
Hamza Agar <hamza.agar@d-teknoloji.com.tr>
2025-07-02 17:07:50 +0300

    Added Updated HandKraft.CQRS.KraftR

---
+++ src/HandKraft.CQRS.KraftR/DependencyInjectionExtensions.cs
@@ -0,0 +1,52 @@
+﻿using HandKraft.Cqrs.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HandKraft.CQRS.KraftR;
+
+public static class DependencyInjectionExtensions
+{
+    /// <summary>
+    /// Registers KraftR mediator and handlers from the specified assemblies.
+    /// </summary>
+    /// <param name="services">Service collection.</param>
+    /// <param name="assemblies">Assemblies to scan for handlers.</param>
+    /// <returns>IServiceCollection for chaining.</returns>
+    public static IServiceCollection AddKraftR(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        // KraftR mediator
+        services.AddSingleton<IKraftR, KraftR>();
+
+        List<TypeInfo> allTypes = assemblies.SelectMany(a => a.DefinedTypes).ToList();
+
+        foreach (TypeInfo type in allTypes)
+        {
+            // Only consider classes that are not abstract
+            if (type.IsClass && !type.IsAbstract)
+            {
+                // All implemented interfaces
+                Type[] interfaces = type.GetInterfaces();
+
+                // Register ICommandHandler<T>
+                IEnumerable<Type> commandHandlerInterfaces = interfaces
+                    .Where(i => i.IsGenericType && (i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>)));
+
+                foreach (Type iface in commandHandlerInterfaces)
+                {
+                    services.AddTransient(iface, type.AsType());
+                }
+
+                // Register IQQueryHandler<T>
+                IEnumerable<Type> qqueryHandlerInterfaces = interfaces
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IQQueryHandler<,>));
+
+                foreach (Type iface in qqueryHandlerInterfaces)
+                {
+                    services.AddTransient(iface, type.AsType());
+                }
+            }
+        }
+
+        return services;
+    }
+}
---
+++ src/HandKraft.CQRS.KraftR/KraftR.cs
@@ -0,0 +1,33 @@
+﻿using HandKraft.Abstractions.Results;
+using HandKraft.Cqrs.Abstractions;
+
+namespace HandKraft.CQRS.KraftR;
+
+public class KraftR(IServiceProvider serviceProvider) : IKraftR
+{
+    private readonly IServiceProvider _serviceProvider = serviceProvider;
+
+    public Task<Result> Send<TRequest>(TRequest command, CancellationToken cancellationToken = default)
+        where TRequest : IRequest<Result>
+    {
+        return _serviceProvider.GetService(typeof(IRequestHandler<TRequest, Result>)) is not IRequestHandler<TRequest, Result> handler
+            ? throw new InvalidOperationException($"Handler not found {typeof(TRequest).Name}")
+            : handler.Handle(command, cancellationToken);
+    }
+
+    public Task<Result<TResponse>> Send<TRequest, TResponse>(TRequest command, CancellationToken cancellationToken = default)
+        where TRequest : IRequest<Result<TResponse>>
+    {
+        return _serviceProvider.GetService(typeof(IRequestHandler<TRequest, Result<TResponse>>)) is not IRequestHandler<TRequest, Result<TResponse>> handler
+            ? throw new InvalidOperationException($"Handler not found {typeof(TRequest).Name}")
+            : handler.Handle(command, cancellationToken);
+    }
+
+    public IQueryable<T> Query<TQQuery, T>(TQQuery query, CancellationToken cancellationToken = default)
+    where TQQuery : IQQuery<T>
+    {
+        return _serviceProvider.GetService(typeof(IQQueryHandler<TQQuery, T>)) is not IQQueryHandler<TQQuery, T> handler
+            ? throw new InvalidOperationException($"Handler not found for {typeof(TQQuery).Name}")
+            : handler.Handle(query);
+    }
+}
